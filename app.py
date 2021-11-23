acdc221df8c5b8b4bfa50d786875ab8255e625b6
udaml-1 <83989624+udaml-1@users.noreply.github.com>
2021-11-23 11:42:01 -0700

    side

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 st.sidebar.title('Ley de Snell')
-st.markdown('''
+st.sidebar.markdown('''
 Software didáctico diseñando
 por los profesores
 * D.C. José Manuel Nápoles Duarte
